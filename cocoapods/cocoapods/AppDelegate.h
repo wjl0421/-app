78ec57165e9e97399fec8eb65dd60e7998b3d8e4
dorayo <yomingyo@gmail.com>
2016-08-12 21:25:10 +0800

    提交

---
+++ cocoapods/cocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cocoapods
+//
+//  Created by qingyun on 16/7/23.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cocoapods/cocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoapods
+//
+//  Created by qingyun on 16/7/23.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
